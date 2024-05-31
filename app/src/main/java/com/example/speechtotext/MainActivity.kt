46374203f63e4c96b5dd29e9764c98a65ef1aaaf
unknown <rajesh.a0408@gmail.com>
2024-05-31 21:46:18 +0530

    final commit

--- app/src/main/java/com/example/speechtotext/MainActivity.kt
+++ app/src/main/java/com/example/speechtotext/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.example.speechtotext
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+    private val RQ_SPEECH_REC = 102
+    lateinit var btn: Button
+    lateinit var tv: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.btn)
+        tv= findViewById(R.id.textView)
+
+        btn.setOnClickListener {
+            askSpeechInput()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode==RQ_SPEECH_REC && resultCode==Activity.RESULT_OK) {
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            tv.text = result?.get(0).toString()
+        }
+    }
+
+    private fun askSpeechInput() {
+        if(!SpeechRecognizer.isRecognitionAvailable(this)) {
+            Toast.makeText(this, "Speech Recognition is not available",Toast.LENGTH_SHORT).show()
+        } else {
+            val i = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
+            i.putExtra(RecognizerIntent.EXTRA_PROMPT,"Say Something!")
+            startActivityForResult(i,RQ_SPEECH_REC)
+        }
     }
 }
